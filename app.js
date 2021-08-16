625704f9a13daab10018b3b2db1fee894a2595e9
luciussulla <maciek.kowalczyk@yandex.com>
2021-08-16 06:32:16 +0200

    Finish banner into slideshow transition

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 
+document.querySelector('.banner button').addEventListener('click', ()=> {
+  document.querySelector('.banner').style.display = 'none'
+  document.querySelector('.container').style.backgroundColor = "none"
+  document.querySelector('.slideshow-wrapper').style.cssText = 'opacity:1;visibility:visible'
+})
